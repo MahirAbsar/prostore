f4d5eee0f2d69a0f3e473b512f5f031533cbe8d6
Mahir <mahirabsar181@gmail.com>
2025-10-20 18:39:04 +0600

    feat: implement product page ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,20 @@
+import ProductList from "@/components/product/product-list";
 import { APP_NAME } from "@/lib/constants";
+import sampleData from "@/db/sample-data";
 
 export const metadata = {
   title: "Home",
 };
 const Homepage = () => {
-  return <>{APP_NAME}</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
